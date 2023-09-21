84b32c3d08bbb51ffe2e7516a87a0fa84c48ca6e
Geethapriya Nagarajan <geethapriya.n@gavstech.com>
2023-09-21 12:12:17 +0530

    first

---
+++ FirstWebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        //api/calculator/add?x=10&y=2
+        [HttpGet("Calculator/Add")]  
+        public int Add(int x , int y)
+        {
+            return x + y;
+        }
+        [HttpGet("Calculator/Sum")]
+        public int Sum(int x, int y)
+        {
+            return x + y+500;
+        }
+        //api/calculator/subtract?x=5&y=3
+        [HttpPost("Calculator/Subtract")]
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+        //api/calculator/multiply?x=5&y=3
+        [HttpPut("Calculator/Multiply")]
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        //api/calculator/divide?x=15&y=3
+        [HttpDelete("Calculator/Divide")]
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
